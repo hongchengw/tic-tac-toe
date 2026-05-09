3fb3a1b66574089b05f5c6f879539c0195961462
Hong Cheng Wang <hongcheng368@gmail.com>
2026-05-09 18:01:33 -0400

    makeMove() Temporary Solution

--- ttt.ts
+++ ttt.ts
@@ -1,29 +1,40 @@
 
 
 class TicTacToe {
-    // properties
+    // Properties
     private readonly board: (string | null)[][] = [ 
-        ["X", "X", "X"], // [0][0, 1, 2]
-        ["X", "X", "X"], // [1][0, 1, 2]
-        ["X", "X", "X"]  // [2][0, 1, 2]
+        [null, null, null], // [0][0, 1, 2]
+        [null, null, null], // [1][0, 1, 2]
+        [null, null, null]  // [2][0, 1, 2]
     ];
 
+    // Constructor
     constructor() { 
         console.log("Board Initialized");
     } 
 
-    public makeMove(row: number, col: number): boolean {
+    // Player adds a move on the board
+    public makeMove(row: number, col: number, mark: string): boolean {
+       // Implement Try & Catch later
         // out of bounds edge casess    
-        // ... make a check for winner method
-        if (this.board[row][col] === null) return false;
-        return true;
+        if (row < 0 || row > 2 || col < 0 || col > 2) return false;
+        const value = this.board[row]?.[col] as string | null;
+        // need to track which player is playing :doro_thinking:
+        if (value === null) { 
+            this.board[row]![col] = mark; 
+            return true; 
+        } else { 
+            console.log("Invalid Positon"); 
+            return false; 
+        }
     }
 
-    // placeholder
+    // placeholder // ... make a check for winner method
     private checkIfWon(row: number, col: number): boolean {
         return true;
     }
 
+    // Displays TicTacToe board in 3x3 Pattern.
     public displayBoard(): void {
         console.log("-------------");
         for (const row of this.board) {
@@ -38,9 +49,22 @@ class TicTacToe {
     }
 } 
 
+
+// Main
 function main(): void {
     const newTTT = new TicTacToe(); 
-    newTTT.displayBoard();
+    /*
+        Start Game: Player 1 (x) & Player 2 (o)
+        Worry about scalability later, hardcode player 1 to be X and player 2 to be O
+    */
+   // Player 1: boolean = true;     // Mark = "X"
+   // Player 2: boolean = false;    // Mark = "O"
+   newTTT.makeMove(0, 1, "X");
+   newTTT.displayBoard();
+   newTTT.makeMove(0, 2, "0");
+   newTTT.displayBoard();
+
+
 }
 
 main();

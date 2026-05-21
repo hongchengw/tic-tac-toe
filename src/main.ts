7476a38a56359faaedc8ca8eb2e94d753618dcfe
Hong Cheng Wang <hongcheng368@gmail.com>
2026-05-21 16:35:47 -0400

    Added continous playback looping

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import { GameManager } from "./GameManager";
+import * as readline from "node:readline";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+function getUserInput(prompt: string): Promise<string> {
+    return new Promise((resolve) => {
+        rl.question(prompt, (answer: string) => {
+            resolve(answer.trim());
+        })
+    })
+}
 
 async function main(): Promise<void> {
+
     const gameManager = new GameManager();
-    await gameManager.start();
+    let playAgain: boolean = true;
+    
+    console.log("Welcome to TicTacToe! (Note: Player 'X' goes first by default.)");
+    
+    while(playAgain) {
+        await gameManager.start();
+        const response = await getUserInput("Play again? (y/n): ");
+        playAgain = response.toLowerCase() === "y";
+
+        if(playAgain) {
+            gameManager.reset();
+            console.log("New Game!");
+        } else {
+            console.log("Thanks for playing!");
+        }
+    }
+
+    rl.close();
 }
 
 main();
